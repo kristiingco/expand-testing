58d5aace55d977bc34a41a78fe26264435a4d899
Kristi Ingco <kristiana.d.ingco@gmail.com>
2024-06-24 15:53:44 -0700

    Add tests for drag and drop

---
+++ tests/dragAndDrop.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+test("browser information", async ({ page }) => {
+    await page.goto("https://practice.expandtesting.com/drag-and-drop");
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(
+        /Drag and Drop page for Automation Testing Practice/
+    );
+
+    let columns = page.locator("#dnd-columns").locator("div");
+
+    await expect(columns.first()).toHaveText("A");
+    await expect(columns.last()).toHaveText("B");
+
+    const columnA = page.locator("#column-a");
+    const columnB = page.locator("#column-b");
+
+    await columnA.dragTo(columnB);
+
+    columns = page.locator("#dnd-columns").locator("div");
+
+    await expect(columns.first()).not.toHaveText("A");
+    await expect(columns.last()).not.toHaveText("B");
+});
